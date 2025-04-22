064dbcc9c140c39afd3ada92bc85e2d269ba130f
Piyushrathoree <meonpath008@gmail.com>
2025-04-22 22:37:15 +0530

    fix: Update root route response to return a plain text message

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
-import express from "express";
-import dotenv from "dotenv";
-import bodyParser from "body-parser";
-import cors from "cors";
-import connectDB from "./src/config/db.js";
+    import express from "express";
+    import dotenv from "dotenv";
+    import bodyParser from "body-parser";
+    import cors from "cors";
+    import connectDB from "./src/config/db.js";
 
-import authRoutes from "./src/routes/auth.route.js";
+    import authRoutes from "./src/routes/auth.route.js";
 
-dotenv.config();
-const app = express();
+    dotenv.config();
+    const app = express();
 
-// Middleware
-const allowedOrigins = ["http://localhost:5000"];
+    // Middleware
+    const allowedOrigins = ["http://localhost:5000"];
 
-const corsOptions = {
-    origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"), false);
-        }
-    },
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-    credentials: true,
-};
+    const corsOptions = {
+        origin: (origin, callback) => {
+            if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+                callback(null, true);
+            } else {
+                callback(new Error("Not allowed by CORS"), false);
+            }
+        },
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+        credentials: true,
+    };
 
-app.use(cors(corsOptions));
-app.use(bodyParser.json());
+    app.use(cors(corsOptions));
+    app.use(bodyParser.json());
 
-// Connect to the database
-connectDB();
-
+    // Connect to the database
+    connectDB();
 app.get("/", (req, res) => {
-    res.send("<h1> server is running</h1>");
+    res.status(200).send("server is running");
 });
-app.use("/api/auth", authRoutes);
 
-// Root route
+    app.use("/api/auth", authRoutes);
 
-const PORT = process.env.PORT;
-app.listen(PORT, () =>
-    console.log(`Server running on http://localhost:${process.env.PORT}`)
-);
+    // Root route
+
+    const PORT = process.env.PORT;
+    app.listen(PORT, () =>
+        console.log(`Server running on http://localhost:${process.env.PORT}`)
+    );
